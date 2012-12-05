9dd6df09abb156e673353b1236cdcf2726848ec8
Karthik <schint5@uic.edu>
2012-12-05 14:32:38 -0600

    Student.drl,SeniorCtizens.drl added

--- src/main/java/com/taxhouse/model/Format.java
+++ src/main/java/com/taxhouse/model/Format.java
@@ -32,22 +32,31 @@ public class Format {
 		// double taxCal = 0;
 		// double exmpPer = 60;
 		//
-		// taxCal = Utils.calculatePercentage(item.getTax(), exmpPer);
-		// item.setTax(item.getTax() - taxCal);
-		// taxRecord.addEntry(new Entry("Tax Exemption Rebate (Personnel Previously worked in A2 Combat Zone)",
+		// taxCal = Utils.calculatePercentage(taxRecord.getTotalTax(), exmpPer);
+		// item.setTax(taxRecord.getTotalTax() - taxCal);
+		// taxRecord.addEntry(new
+		// Entry("Tax Exemption Rebate (Personnel Previously worked in A2 Combat Zone)",
 		// exmpPer, -taxCal));
 		// }
 
 		Investment investment;
 		Exemption exemption;
 
-		double amount = taxRecord.getTotalTax();
-		double percent = 0.5;
+		double amount = item.getTaxableIncome();
+		double percent = 0;
+
+		if (item.getState().substring(0, 1) == "A"
+				|| item.getState().substring(0, 1) == "B") {
+			percent = 8;
+		} else if (item.getState().substring(0, 1) == "C"
+				|| item.getState().substring(0, 1) == "D") {
+			percent = 9;
+		} else {
+			percent = 7;
+		}
 
 		double fraction = Utils.calculatePercentage(amount, percent);
-		taxRecord.addEntry(new Entry("Widow Senior Citizen Exemption", percent,
-
-		fraction));
+		taxRecord.addEntry(new Entry("Base State tax for Graduate Assistants @ ", percent,fraction));
 
 	}
 }
