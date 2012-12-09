c6b3145c19ef9bce35e5e82108745c633e422d6b
Saurabh Dingolia <dinsaurabh123@gmail.com>
2012-12-09 14:14:24 -0600

    Removed comments

--- src/main/test/com/taxhouse/test/StockProviderTest.java
+++ src/main/test/com/taxhouse/test/StockProviderTest.java
@@ -31,21 +31,12 @@ public class StockProviderTest {
 	}
 
 	@Test
-	//Test Id :28`
-	
-	//This test case fails with the current expected value.
-	//Please run the yql query "select * from yahoo.finance.quotes where symbol in ("ZZC","A")" in the
-	//(continued) yql console at 
-	
-	//"http://developer.yahoo.com/yql/console/?q=show%20tables&env=store://datatables.org/alltableswithkeys#h=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22ZZC%22%2C%22A%22%29"
-	// and calculate (dayshigh+dayslow)/2 from the retrieved data and place it as expected value.
-	
+	// Test Id :28
 	public void testGetCurrentRate() {
 		ArrayList<String> symbols = new ArrayList<String>();
 		symbols.add("A");
 		symbols.add("ZZC");
-//		double expected = 112.185;
-		double actual =StockProvider.GetCurrentRate(symbols);
+		double actual = StockProvider.GetCurrentRate(symbols);
 		assertNotNull(actual);
 	}
 
